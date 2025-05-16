b59c7121038e5dc379fb257354aaf796624e57ee
moussasamina <moussa.malle@samina.tech>
2025-05-16 11:19:52 +0000

    change cp to cpx

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,3 @@
-// import { PrismaClient as PrismaClientNewDb } from "@/generated/prisma_new_db"
-// import { PrismaClient as PrismaClientRootDb } from "@/generated/prisma_root_db"
-// import { PrismaClient } from "@prisma/client"
-
 import { PrismaClient as PrismaClientYoobuWP } from "../generated/prisma_yb_wp/index.js";
 import { PrismaClient as PrismaClientYoobuLogistic } from "../generated/prisma_yb_logistic/index.js";
 
